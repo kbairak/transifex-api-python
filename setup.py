4143d71e133bec1fe99f748afb05951e87c27e7c
Konstantinos Bairaktaris <kbairak@transifex.com>
2020-09-22 09:30:47 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 setup(name="transifex_api",
       version="0.0.1",
       install_requires=["requests"],
-      packages=find_packages(),
+      packages=find_packages('src'),
       package_dir={'': 'src'})
