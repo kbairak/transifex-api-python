a14a88270fe846d1df3a34cb6e944a0ce8674731
Konstantinos Bairaktaris <kbairak@transifex.com>
2020-09-22 09:32:09 +0300

    Add 'six' to requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import find_packages, setup
 
 setup(name="transifex_api",
       version="0.0.1",
-      install_requires=["requests"],
+      install_requires=["requests", "six"],
       packages=find_packages('src'),
       package_dir={'': 'src'})
